b5de09f268bfc3a7c2e751fd8f3461a6837b5e28
Kwanwoo Kim <kim.kw8012@gmail.com>
2023-07-14 15:37:19 -0700

    notion added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,29 @@
 <template>
-  <div></div>
+  <NotionRenderer v-if="data" :blockMap="data" fullPage />
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+import { defineComponent, onMounted } from "vue";
+import { NotionRenderer, getPageBlocks, useGetPageBlocks } from "vue3-notion";
 
-@Options({
-  components: {},
-})
-export default class HomeView extends Vue {}
+import "@/styles/notion/notion.css";
+
+export default defineComponent({
+  components: {
+    NotionRenderer,
+  },
+  setup() {
+    const { data } = useGetPageBlocks("4b2dc28a5df74034a943f8c8e639066a");
+    onMounted(async () => {
+      data.value = await getPageBlocks("4b2dc28a5df74034a943f8c8e639066a");
+    });
+
+    return { data };
+  },
+});
 </script>
+
+<style>
+@import "prismjs/themes/prism.css";
+@import "katex/dist/katex.min.css";
+</style>
